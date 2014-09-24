92e3ba73463396106cda3fd9f687f62c781207e0
Tugaev Maxim <tugmaks@yandex.ru>
2014-09-24 17:11:37 +0400

    register google core JS

--- Map.php
+++ Map.php
@@ -11,7 +11,24 @@ class Map extends \yii\base\Widget {
     public function init() {
         $api_key = Yii::$app->params['GOOGLE_API_KEY'];
         $sensor = $this->sensor ? 'true' : 'false';
-        $this->view->registerJsFile('https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&sensor=' . $sensor);
+        //$this->view->registerJsFile('https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&sensor=' . $sensor);
+        $this->view->registerJs('function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&sensor=' . $sensor . '&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;');
         parent::init();
     }
 
--- views/map.php
+++ views/map.php
@@ -1,2 +1,2 @@
-map
+<div id="map_canvas" style="width:100%; height:100%"></div>
 
