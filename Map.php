f76bba9e940a6bf38c54f4e93d6481b4d0ef74ff
Tugaev Maxim <tugmaks@yandex.ru>
2014-09-24 16:30:17 +0400

    register google core JS

--- Map.php
+++ Map.php
@@ -11,7 +11,8 @@ class Map extends \yii\base\Widget {
 
     public function init() {
         $api_key = Yii::$app->params['GOOGLE_KEY_API'];
-        $this->view->registerJsFile('https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&sensor=' . $this->sensor);
+        $sensor = $this->sensor ? 'true' : 'false';
+        $this->view->registerJsFile('https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&sensor=' . $sensor);
         FontAwesomeAsset::register($this->view);
         parent::init();
     }
