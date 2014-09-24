3fdbd7664e53335de3f8f290c09aa54602b3d56c
Tugaev Maxim <tugmaks@yandex.ru>
2014-09-24 17:51:13 +0400

    register google core JS

--- Map.php
+++ Map.php
@@ -15,27 +15,28 @@ class Map extends \yii\base\Widget {
         $api_key = Yii::$app->params['GOOGLE_API_KEY'];
         $sensor = $this->sensor ? 'true' : 'false';
         //$this->view->registerJsFile('https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&sensor=' . $sensor);
-        $this->view->registerJs('function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  codeAddress("' . $this->address . '");
-}
-function codeAddress(address) {
-geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { "address": address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      }
+        $this->view->registerJs('
+var geocoder, map;            
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        "address": address
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var myOptions = {
+                zoom: 8,
+                center: results[0].geometry.location,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        }
     });
-    }
+}
 function loadScript() {
   var script = document.createElement("script");
   script.type = "text/javascript";
