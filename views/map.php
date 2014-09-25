3bb0c8a9bed3ea444118783e986b1f20f0e1ea9a
Tugaev Maxim <tugmaks@yandex.ru>
2014-09-25 14:48:44 +0400

    Readme and Map

--- views/map.php
+++ views/map.php
@@ -12,20 +12,20 @@
             center: false
         };
 <?php if (is_array($this->context->center)): ?>
-            mapOptions[center] = new google.maps.LatLng(<?= $this->context->center[0] ?>, <?= $this->context->center[1] ?>);
+            mapOptions['center'] = new google.maps.LatLng(<?= $this->context->center[0] ?>, <?= $this->context->center[1] ?>);
 <?php else: ?>
             geocoder.geocode({
                 "address": "<?= $this->context->center ?>"
             }, function (results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
-                    mapOptions[center] = results[0].geometry.location;
+                    mapOptions['center'] = results[0].geometry.location;
                 }
 
             }
 
             );
 <?php endif; ?>
-alert(mapOptions.center);
+        alert( mapOptions['center']);
 
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
