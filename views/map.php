93ca287d970745c09cc65fc64f9720cfb3ae4b22
Tugaev Maxim <tugmaks@yandex.ru>
2014-09-25 15:25:06 +0400

    Readme and Map

--- views/map.php
+++ views/map.php
@@ -1,6 +1,7 @@
 <div style="width: <?= $this->context->width ?>px;height: <?= $this->context->height ?>px">
     <div id="map_canvas" style="width:100%; height:100%"></div>
 </div>
+<?php var_dump(is_array($this->context->center)) ?>
 <script>
     var geocoder, map;
 
