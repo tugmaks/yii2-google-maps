0bc0d53857020acf291070bcb67e4c97723d126f
Maksim Tugaev <tugmaks@yandex.ru>
2014-09-25 21:26:52 +0400

    test map

--- views/map.php
+++ views/map.php
@@ -2,30 +2,31 @@
     <div id="map_canvas" style="width:100%; height:100%"></div>
 </div>
 <script>
-    var mapCenter;
     function initialize() {
         var geocoder = new google.maps.Geocoder(),
                 map;
 <?php if (is_array($this->context->center)): ?>
-            mapCenter = new google.maps.LatLng(<?= $this->context->center[0] ?>, <?= $this->context->center[1] ?>);
+            map = new google.maps.Map(document.getElementById("map_canvas"),
+                    {
+                        zoom: <?= $this->context->zoom ?>,
+                        mapTypeId: google.maps.MapTypeId.<?= $this->context->mapType ?>,
+                        center: new google.maps.LatLng(<?= $this->context->center[0] ?>, <?= $this->context->center[1] ?>)
+                    }
+            );
 <?php else: ?>
             geocoder.geocode({
                 "address": "<?= $this->context->center ?>"
             }, function(results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
-                    alert('0status ok, set center');
-                    setMapCenter(results[0].geometry.location);
+                    map = new google.maps.Map(document.getElementById("map_canvas"), {
+                        zoom: <?= $this->context->zoom ?>,
+                        mapTypeId: google.maps.MapTypeId.<?= $this->context->mapType ?>,
+                        center: results[0].geometry.location
+                    });
                 }
             });
 <?php endif; ?>
-        alert('2. init here');
-        map = new google.maps.Map(document.getElementById("map_canvas"),
-                {
-                    zoom: <?= $this->context->zoom ?>,
-                    mapTypeId: google.maps.MapTypeId.<?= $this->context->mapType ?>,
-                    center: window.mapCenter
-                }
-        );
+
 //        var marker = new google.maps.Marker({
 //            map: map,
 //            position: results[0].geometry.location
@@ -36,10 +37,6 @@
         script.type = "text/javascript";
         script.src = "https://maps.googleapis.com/maps/api/js?key=<?= $this->context->apiKey ?>&sensor=<?= $this->context->sensor ?>&callback=initialize";
         document.body.appendChild(script);
-    }
-    function setMapCenter(center) {
-        alert('1. setting here');
-        window.mapCenter = center;
     }
     window.onload = loadScript;
 </script>
